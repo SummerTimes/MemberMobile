2789e86f5f255515970f2c99d8fd0721eb78c387
yangdong <1039734767@qq.com>
2019-10-12 14:33:39 +0800

    优化代码

--- login/src/main/java/com/kk/app/login/activity/LoginActivity.java
+++ login/src/main/java/com/kk/app/login/activity/LoginActivity.java
@@ -17,6 +17,9 @@ import com.kk.app.login.bean.CommonBean;
 import com.kk.app.login.bean.CommonList;
 import com.kk.app.login.util.LoginUtils;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,13 +44,7 @@ public class LoginActivity extends AppCompatActivity {
      * 初始化View
      */
     private void initView() {
-        mSivPic = findViewById(R.id.siv_pic);
-        mSivPic.load("http://seopic.699pic.com/photo/50055/5642.jpg_wh1200.jpg");
 
-        mUrls = new ArrayList<>();
-        mUrls.add("http://seopic.699pic.com/photo/50055/5642.jpg_wh1200.jpg");
-        mUrls.add("http://seopic.699pic.com/photo/50055/5642.jpg_wh1200.jpg");
-        mUrls.add("http://seopic.699pic.com/photo/50055/5642.jpg_wh1200.jpg");
     }
 
     /**
@@ -56,26 +53,30 @@ public class LoginActivity extends AppCompatActivity {
      * @param view
      */
     public void onCheckNet(View view) {
-        String Url = "https://wanandroid.com/wxarticle/chapters/json";
-        NetworkHelper.queryApi(Url, null, NetworkHelper.HTTP_GET, new NetworkCallback<CommonList>() {
-            @Override
-            public void onSuccess(@NonNull CCResult rawResult, CommonList commonList) {
-                Log.e("xp", "---onSuccess----" + commonList.toString());
-                List<CommonBean> data = commonList.data;
-                for (int i = 0; i < data.size(); i++) {
-                    Log.e("xp", "---单条数据----" + data.get(i).toString());
+        String Url = "https://www.wanandroid.com/user/login";
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("username", "lxtimes");
+            jsonObject.put("password", "123456789");
+
+            NetworkHelper.queryApi(Url, null, NetworkHelper.HTTP_POST, new NetworkCallback<CommonList>() {
+                @Override
+                public void onSuccess(@NonNull CCResult rawResult, CommonList commonList) {
+                    Log.e("xp", "---onSuccess----" + commonList.toString());
                 }
-            }
 
-            @Override
-            public void onFailed(@NonNull CCResult result) {
-                Log.e("xp", "---onFailed----" + result.getData());
-            }
+                @Override
+                public void onFailed(@NonNull CCResult result) {
+                    Log.e("xp", "---onFailed----" + result.getData());
+                }
 
-            @Override
-            public void onFinally(@NonNull CCResult result) {
-                Log.e("xp", "---onFinally----" + result.getData());
-            }
-        });
+                @Override
+                public void onFinally(@NonNull CCResult result) {
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
